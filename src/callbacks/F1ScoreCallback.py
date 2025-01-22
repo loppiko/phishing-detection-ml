b7287cdcb47e5953148a5acc5902a56f882d495e
loppiko <insertthemontana@gmail.com>
2025-01-22 09:21:59 +0100

    adding notebooks

--- src/callbacks/F1ScoreCallback.py
+++ src/callbacks/F1ScoreCallback.py
@@ -14,3 +14,17 @@ class F1ScoreCallback(Callback):
         y_true = np.argmax(self.y_val, axis=1)
         f1 = f1_score(y_true, y_pred, average='weighted')
         self.f1_scores.append(f1)
+
+
+
+class F1ScoreCallbackBert(Callback):
+    def __init__(self, val_dataset):
+        val_data = list(val_dataset.as_numpy_iterator())
+        self.x_val = {key: np.stack([item[0][key] for item in val_data]) for key in val_data[0][0].keys()}
+        self.y_val = np.array([item[1] for item in val_data])
+        self.f1_scores = []
+
+    def on_epoch_end(self, epoch, logs=None):
+        y_pred = np.argmax(self.model.predict(self.x_val).logits, axis=1)
+        f1 = f1_score(self.y_val, y_pred, average='weighted')
+        self.f1_scores.append(f1)
--- src/models/bert/bert-v1-model.py
+++ src/models/bert/bert-v1-model.py
@@ -1,5 +1,3 @@
-from platform import win32_edition
-
 import pandas as pd
 
 import tensorflow as tf
@@ -10,20 +8,24 @@ from transformers.tokenization_utils_fast import PreTrainedTokenizerFast
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from transformers import TFAutoModelForSequenceClassification
 
+from collections import Counter
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.utils.class_weight import compute_class_weight
 from tensorflow.python.data.ops.dataset_ops import DatasetV2
 from tensorflow.keras.callbacks import History
+from imblearn.over_sampling import RandomOverSampler
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 from src.models.model import Model
+from src.callbacks.F1ScoreCallback import F1ScoreCallbackBert
 
 
 
-MAIN_DIR = "/content/model/"
+MAIN_DIR = "/home/loppiko/Documents/studies/5-sem/NLP/project/src/"
 
 
 class BertModel(Model):
@@ -31,6 +33,7 @@ class BertModel(Model):
     tokenizer: PreTrainedTokenizerFast
     history: History
     cm: list[list[float]] | None = None
+    f1_scores: list[float] = []
 
     def __init__(self, model_type: str, model_name: str) -> None:
         self.model_name = model_name
@@ -45,10 +48,20 @@ class BertModel(Model):
             loss=SparseCategoricalCrossentropy(from_logits=True),
             metrics=['accuracy'])
 
+        early_stopping = tf.keras.callbacks.EarlyStopping(
+            monitor='val_loss',
+            patience=10,
+            restore_best_weights=True
+        )
+
+        f1_callback = F1ScoreCallbackBert(val_dataset)
+
         if class_weights_dict:
-            self.history = self.model.fit(train_dataset, validation_data=val_dataset, epochs=3, class_weight=class_weights_dict)
+            self.history = self.model.fit(train_dataset, validation_data=val_dataset, epochs=3, class_weight=class_weights_dict, callbacks=[f1_callback])
         else:
-            self.history = self.model.fit(train_dataset, validation_data=val_dataset, epochs=3)
+            self.history = self.model.fit(train_dataset, validation_data=val_dataset, epochs=3, callbacks=[f1_callback, early_stopping])
+
+        self.f1_scores = f1_callback.f1_scores
 
 
     def predict(self, to_predict: list[list[str]]) -> np.ndarray:
@@ -56,7 +69,6 @@ class BertModel(Model):
         test_dataset = tf.data.Dataset.from_tensor_slices(dict(tokenized_data)).batch(8)
         logits = self.model.predict(test_dataset).logits
 
-        # Konwersja logitów na etykiety (największa wartość logitu dla każdej próbki)
         return tf.argmax(logits, axis=1).numpy()
 
 
@@ -87,7 +99,7 @@ class BertModel(Model):
     def make_plots(self) -> None:
         history = self.history.history
 
-        plt.figure(figsize=(12, 6))
+        plt.figure(figsize=(18, 6))
         plt.subplot(1, 2, 1)
         plt.plot(history['accuracy'], label='Train Accuracy')
         plt.plot(history['val_accuracy'], label='Validation Accuracy')
@@ -104,13 +116,21 @@ class BertModel(Model):
         plt.ylabel('Loss')
         plt.legend()
 
+        plt.subplot(1, 3, 3)
+        plt.plot(self.f1_scores, label='Validation F1-score')
+        plt.title('F1-score Over Epochs')
+        plt.xlabel('Epoch')
+        plt.ylabel('F1-score')
+        plt.legend()
+
         plt.tight_layout()
         plt.savefig(f"{MAIN_DIR}saved-results/bert/{self.model_name}-accuracy.png")
 
         if self.cm is not None:
             plt.figure(figsize=(8, 6))
-            labels = list(self.model.config.id2label.values()) if hasattr(self.model.config, "id2label") else None
-            sns.heatmap(self.cm, annot=True, fmt='d', cmap='Blues', xticklabels=labels, yticklabels=labels)
+            sns.heatmap(self.cm, annot=True, fmt='d', cmap='Blues',
+                        xticklabels=['legal', 'spam', 'phishing', 'fraud'],
+                        yticklabels=['legal', 'spam', 'phishing', 'fraud'])
             plt.xlabel('Predicted')
             plt.ylabel('True')
             plt.title('Confusion Matrix')
@@ -118,27 +138,91 @@ class BertModel(Model):
         else:
             print("Cannot save confusion-matrix, because it was not previously created")
 
+        with open(f"{MAIN_DIR}saved-results/lstm/v2/data/{self.model_name}-final-metrics.txt", "w") as file:
+            file.write(f"Accuracy: {history['accuracy'][-1]}\n")
+            file.write(f"F1-Score: {self.f1_scores[-1]}\n")
 
     def train_bert_model(self, train_data: pd.DataFrame, train_labels: pd.DataFrame, max_length: int) -> None:
-        X_train, X_temp, Y_train, Y_temp = train_test_split(train_data.values.tolist(), train_labels.values.tolist(), test_size=0.3, random_state=28)
-        X_val, X_test, Y_val, Y_test = train_test_split(X_temp, Y_temp, test_size=0.5, random_state=28)
+        # Podział danych na zestawy treningowe, walidacyjne i testowe
+        X_train, X_temp, Y_train, Y_temp = train_test_split(
+            train_data.values.tolist(),
+            train_labels.values.tolist(),
+            test_size=0.3,
+            random_state=28
+        )
+        X_val, X_test, Y_val, Y_test = train_test_split(
+            X_temp, Y_temp, test_size=0.5, random_state=28
+        )
+
+        # Oversampling dla danych treningowych
+        oversampler = RandomOverSampler(random_state=28)
+        X_train_resampled, Y_train_resampled = oversampler.fit_resample(np.array(X_train).reshape(-1, 1), Y_train)
+        X_val_resampled, Y_val_resampled = oversampler.fit_resample(np.array(X_val).reshape(-1, 1), Y_val)
+
+        X_train_resampled = X_train_resampled.flatten().tolist()
+        X_val_resampled = X_val_resampled.flatten().tolist()
 
         X_test_df: list[list[str]] = X_test
+        X_train, X_val, X_test = (
+            self.tokenize_data(X_train_resampled, max_length),
+            self.tokenize_data(X_val, max_length),
+            self.tokenize_data(X_test, max_length)
+        )
 
-        X_train, X_val, X_test = self.tokenize_data(X_train, max_length), self.tokenize_data(X_val, max_length), self.tokenize_data(X_test, max_length)
-
-        train_dataset = tf.data.Dataset.from_tensor_slices((dict(X_train), Y_train)).batch(8)
+        # Tworzenie zestawów danych w TensorFlow
+        train_dataset = tf.data.Dataset.from_tensor_slices((dict(X_train), Y_train_resampled)).batch(8)
         val_dataset = tf.data.Dataset.from_tensor_slices((dict(X_val), Y_val)).batch(8)
         test_dataset = tf.data.Dataset.from_tensor_slices((dict(X_test), Y_test)).batch(8)
 
+        train_counter = Counter(Y_train_resampled)
+        val_counter = Counter(Y_val_resampled)
 
-        class_weights = compute_class_weight('balanced', classes=np.unique(train_labels.values), y=labels)
-        class_weights_dict = {i: weight for i, weight in enumerate(class_weights)}
+        print(train_counter)
+        print(val_counter)
 
-        self.fit(train_dataset, val_dataset, class_weights_dict)
-        self.evaluate(test_dataset)
-        self.create_confusion_matrix(X_test_df, np.array(Y_test))
-        self.make_plots()
+        print("X_train_resampled shape:", np.array(X_train_resampled).shape)
+        print("Y_train_resampled shape:", np.array(Y_train_resampled).shape)
+
+        print("X_val_resampled shape:", np.array(X_val).shape)
+        print("Y_val_resampled shape:", np.array(Y_val).shape)
+
+        print("X_test_resampled shape:", np.array(X_test).shape)
+        print("Y_test_resampled shape:", np.array(Y_test).shape)
+
+
+        # Obliczenie wag klas po oversamplingu
+        # class_weights = compute_class_weight(
+        #     class_weight='balanced',
+        #     classes=np.unique(Y_train_resampled),
+        #     y=Y_train_resampled
+        # )
+        # class_weights_dict = {i: weight for i, weight in enumerate(class_weights)}
+        #
+        # # Trenowanie modelu
+        # self.fit(train_dataset, val_dataset, class_weights_dict)
+        #
+        # # Ewaluacja modelu
+        # self.evaluate(test_dataset)
+        # self.create_confusion_matrix(X_test_df, np.array(Y_test))
+        # self.make_plots()
+
+
+
+        # Obliczenie wag klas po oversamplingu
+        # class_weights = compute_class_weight(
+        #     class_weight='balanced',
+        #     classes=np.unique(Y_train_resampled),
+        #     y=Y_train_resampled
+        # )
+        # class_weights_dict = {i: weight for i, weight in enumerate(class_weights)}
+        #
+        # # Trenowanie modelu
+        # self.fit(train_dataset, val_dataset, class_weights_dict)
+        #
+        # # Ewaluacja modelu
+        # self.evaluate(test_dataset)
+        # self.create_confusion_matrix(X_test_df, np.array(Y_test))
+        # self.make_plots()
 
 
 MODEL_TYPE = "bert-base-uncased"
